e7fd869ea429dd2f4287dd5d05f79e6619b0cc59
John Moeller <fishcorn@gmail.com>
2013-03-13 23:48:07 -0600

    Using explicit datatype constructors for clarity

--- python/testTestMKL.py
+++ python/testTestMKL.py
@@ -25,29 +25,29 @@ class TestMKL1TestCase(unittest.TestCase):
 
     def testSimpleMargin1(self):
         "Test against two 1-dimensional support points (SIGN), 1 kernel (linear)"
-        Sigma = np.array([1.]); # 1 kernel, weight 1.0
-        alpha = np.array([1.,1.])/2; # 2 support points of equal weight
-        kerns = np.array([0], dtype='int32'); # 1 linear kernel
-        params = np.array([0.]); # params not important
-        features = np.array([-1], dtype='int32'); # use all features, but there's only 1
-        Xtr = np.array([[-2.,2.]]); # support points
+        Sigma = np.double([1.]); # 1 kernel, weight 1.0
+        alpha = np.double([1.,1.])/2; # 2 support points of equal weight
+        kerns = np.int32([0]); # 1 linear kernel
+        params = np.double([0.]); # params not important
+        features = np.int32([-1]); # use all features, but there's only 1
+        Xtr = np.double([[-2.,2.]]); # support points
         Xte = Xtr/2; # test points
-        ytr = np.array([-1,1], dtype='int32'); # labels
+        ytr = np.int32([-1,1]); # labels
         results = test_mkl(Sigma, alpha, kerns, params, features, Xtr, Xte, ytr)
         self.assertTrue((np.sign(results) == ytr).all(), 
                         msg='results={0}, ytr={1}'.format(results, ytr))
 
     def testSimpleMargin2(self):
         "Test against four 2-dimensional support points (XOR), 1 kernel (quad)"
-        Sigma = np.array([1.]); # 1 kernel, weight 1.0
-        alpha = np.array([1.,1.,1.,1.])/4; # 4 support points of equal weight
-        kerns = np.array([1], dtype='int32'); # 1 polynomial kernel
-        params = np.array([2.]); # quadratic
-        features = np.array([-1], dtype='int32'); # use all features
-        Xtr = np.array([[-2.,2.,-2.,2.],
-                        [-2.,-2.,2.,2.]]); # support points
+        Sigma = np.double([1.]); # 1 kernel, weight 1.0
+        alpha = np.double([1.,1.,1.,1.])/4; # 4 support points of equal weight
+        kerns = np.int32([1]); # 1 polynomial kernel
+        params = np.double([2.]); # quadratic
+        features = np.int32([-1]); # use all features
+        Xtr = np.double([[-2.,2.,-2.,2.],
+                         [-2.,-2.,2.,2.]]); # support points
         Xte = Xtr/2; # test points
-        ytr = np.array([-1,1,1,-1], dtype='int32'); # labels
+        ytr = np.int32([-1,1,1,-1]); # labels
         results = test_mkl(Sigma, alpha, kerns, params, features, Xtr, Xte, ytr)
         self.assertTrue((np.sign(results) == ytr).all(), 
                         msg='results={0}, ytr={1}'.format(results, ytr))
@@ -61,11 +61,11 @@ class TrainMKL1TestCase(unittest.TestCase):
 
     def testSimpleTrain1(self):
         "Two 1-dimensional input points (SIGN), 1 kernel (linear)"
-        kerns = np.array([0], dtype='int32'); # 1 linear kernel
-        params = np.array([0.]); # params not important
-        features = np.array([-1], dtype='int32'); # use all features, but there's only 1
-        Xtr = np.array([[-2.,2.]]); # support points
-        ytr = np.array([-1,1], dtype='int32'); # labels
+        kerns = np.int32([0]); # 1 linear kernel
+        params = np.double([0.]); # params not important
+        features = np.int32([-1]); # use all features, but there's only 1
+        Xtr = np.double([[-2.,2.]]); # support points
+        ytr = np.int32([-1,1]); # labels
         (success, Sigma, alpha, bsvm, posw) = train_mwu_mkl(kerns, params, features, Xtr, ytr)
         self.assertTrue(success)
         self.assertTrue((posw == 1).all(), msg='posw={0}'.format(posw))
@@ -75,12 +75,12 @@ class TrainMKL1TestCase(unittest.TestCase):
 
     def testSimpleTrain2(self):
         "Four 2-dimensional input points (XOR), 1 kernel (quad)"
-        kerns = np.array([1], dtype='int32'); # 1 polynomial kernel
-        params = np.array([2.]); # quadratic
-        features = np.array([-1], dtype='int32'); # use all features, but there's only 1
-        Xtr = np.array([[-2.,2.,-2.,2.],
-                        [-2.,-2.,2.,2.]]); # support points
-        ytr = np.array([-1,1,1,-1], dtype='int32'); # labels
+        kerns = np.int32([1]); # 1 polynomial kernel
+        params = np.double([2.]); # quadratic
+        features = np.int32([-1]); # use all features, but there's only 1
+        Xtr = np.double([[-2.,2.,-2.,2.],
+                         [-2.,-2.,2.,2.]]); # support points
+        ytr = np.int32([-1,1,1,-1]); # labels
         (success, Sigma, alpha, bsvm, posw) = train_mwu_mkl(kerns, params, features, Xtr, ytr)
         self.assertTrue(success)
         self.assertTrue((posw == 1).all(), msg='posw={0}'.format(posw))
